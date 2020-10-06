b114be77a90bcb06cfa80d72b21967cd3e18719a
Gabriel-J3sus <gabr.jesus001@gmail.com>
2020-10-06 17:24:26 -0300

    third commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const Header = () => {
+export default function Header() {
   return (
     <Container>
       <h1> Pipefy </h1>
     </Container>
   );
 };
-
-export default Header;
